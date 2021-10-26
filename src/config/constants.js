d0694031b1396f8e695a4df7a5484e0d976f173a
SeoyeonKim93 <hiyeeonbly@gmail.com>
2021-10-26 15:43:38 +0900

    vercel 배포 준비

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,6 @@
-export const API_URL = "http://localhost:8080";
+// 실제 돌아가고 있는 API 서버 주소와 (cloud 환경에서는 production이 들어감)
+// 개발할 때 사용하는 localhost 주소
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://seoyeon-market-server.herokuapp.com"
+    : "http://localhost:8080";
