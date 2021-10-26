e397ee76225ce19c9993cbe499cfba5cb613ddef
SeoyeonKim93 <hiyeeonbly@gmail.com>
2021-10-26 14:05:28 +0900

    web 기능 추가

--- src/main/index.js
+++ src/main/index.js
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { API_URL } from "../config/constants.js";
-
+import { Carousel } from "antd";
 dayjs.extend(relativeTime);
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   // useEffect : 안에 있는 첫번째 인자는 두번째 인자가 변하지 않는한 한번만 랜더링 된다.
   // useEffect 안에 axios 안넣어주면 주한대로 setProduct가 통신되는 오류 발생
   React.useEffect(function () {
@@ -28,19 +29,39 @@ function MainPage() {
       .catch(function (error) {
         console.error("에러 발생:", error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러발생 : ", error);
+      });
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageurl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
       <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {/* 배열로 부터 순회를 하면서 return 안을 내보냄 */}
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {/* 결제 완료된 상품 흐리게 만들기 */}
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link
                 style={{ color: "inherit" }}
                 className="product-link"
--- src/product/index.js
+++ src/product/index.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants.js";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  useEffect(function () {
+
+  const getProduct = () => {
     axios
       .get(`${API_URL}/products/${id}`)
       .then(function (result) {
@@ -17,12 +19,31 @@ function ProductPage() {
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  useEffect(function () {
+    getProduct();
   }, []);
   //console.log(product);
 
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   }
+
+  // 구매하기 버튼을 눌렀을 때 페이지 넘어가는 통신 필요
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다.");
+        // 새로고침을 실행해보자
+        getProduct();
+      })
+      .catch((error) => {
+        console.log(error);
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
 
   return (
     <div>
@@ -40,6 +61,17 @@ function ProductPage() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          // 구매 후에는 구매 버튼 비활성화 시키기
+          disabled={product.soldout === 1 ? true : false}
+        >
+          재빨리 구매하기
+        </Button>
         {/* pre 태그를 div 대신 넣으면 줄바꿈까지 보여준다 */}
         <pre id="description">{product.description}</pre>
       </div>
